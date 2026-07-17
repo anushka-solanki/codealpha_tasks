d25ede9d71d56fc230ad780792c3e8ad8d630d75
Anushka Solanki <anushkasolanki448@gmail.com>
2026-07-17 20:28:19 +0530

    Delete src/com/hotel directory

--- src/com/hotel/model/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.model;
-
-public class Admin extends User {
-    private static final long serialVersionUID = 1L;
-
-    public Admin(String username, String password, String fullName) {
-        super(username, password, fullName);
-    }
-}
